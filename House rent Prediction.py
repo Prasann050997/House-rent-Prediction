e84a8d990a864e9e13612287b36de29fdc4a56a7
Patil Prasann <124381560+Prasann050997@users.noreply.github.com>
2023-07-30 22:06:22 +0530

    Add files via upload

---
+++ House rent Prediction.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+
+data = pd.read_csv("House_Rent_Dataset.csv")
+print(data.head())
